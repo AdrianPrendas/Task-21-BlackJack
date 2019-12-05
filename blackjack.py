9a5338a598ca2c1c2a782000607f6f09e8bfb046
Adrian Prendas Araya <adrian.prendas.araya@est.una.ac.cr>
2019-12-05 14:52:25 -0600

    after video call

--- blackjack.py
+++ blackjack.py
@@ -1,76 +1,74 @@
 
 import functools 
+import random
 
 VALUES = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 
 KIND = [
-{"kind":"C"},
-{"kind":"D"},
-{"kind":"T"},
-{"kind":"P"},
+    {"kind":"C"},
+    {"kind":"D"},
+    {"kind":"T"},
+    {"kind":"P"},
 ]
 
-DECK = [
-    {"kind": k["kind"], "value": v} for k in KIND for v in VALUES
-]
-
-
-#for e in DECK:
-#        print(e)
-#    print(len(DECK))
+DECK = [{"kind": k["kind"], "value": v} for k in KIND for v in VALUES]
 
 class Player:
     def __init__(self, n):
         self.n = n
         self.hand = []
 
-    def getCard(self, card):
+    def addCard(self, card):
         self.hand.append(card)
 
     def calculate(self):
         if len(self.hand) < 2:
             return 0
-        return functools.reduce(lambda a,b: a["value"]+b["value"], self.hand)
+        values = list(map(lambda cart: cart["value"] ,self.hand))
+        return functools.reduce(lambda a,b: a+b, values)
 
     def getMore(self):
         return True if self.calculate() < 16 else False
 
-    def __str__(self):
-        return f"{self.hand}"
-    
-        
+    def getTotal(self):
+        values = list(map(lambda cart: cart["value"] ,self.hand))
+        return functools.reduce(lambda a,b: a+b, values)
 
+    def __str__(self):
+        return f"id:{self.n} =>{self.hand}=> total: {self.getTotal()}"
+    
 
 
 
 if __name__ == "__main__":
-    n = input("how many players you want: ")
-    n = int(n)
+    
+    random.shuffle(DECK) #sorting the DECK to get random cart
+
+    n = int(input("how many players you want: "))
+    
+    house = Player("house")
     players = []
-    house = Player(666)
 
-
-    #init players
     for i in range(n):
         players.append(Player(i))
-
 
     while True:
         for i in range(n):
             if players[i].getMore():
                 cart = DECK.pop()
                 print("cart: ", cart)
-                players[i].getCard(cart)
+                players[i].addCard(cart)
         if house.getMore():
             cart = DECK.pop()
             print("cart: ", cart)
-            house.getCard(cart)
+            house.addCard(cart)
 
         more = list(map(lambda p: p.getMore(),players))
         more = list(map(lambda s: not s ,more))
         if all(more) and not house.getMore():
             break
     
+    print("\n\n")
     print("players")
     for e in players:
         print(e)
@@ -79,19 +77,15 @@ if __name__ == "__main__":
     print("house")
     print(house)
 
-        
-        
-        
-            
+    filtering = [p for p in players if p.getTotal() <= 21]
+    if house.getTotal() <= 21: 
+        filtering.append(house)
+
+    winner = max(filtering, key=lambda p:p.getTotal())
 
 
+    print("\n\n")
+    print("winner :", winner)
 
-
-    #p = Player(1)
-    #p.getCard(DECK[len(DECK)-1])
-    #p.getCard(DECK[len(DECK)-2])
-    #print(p.getMore())
-
-    
 
     
